c8245a8a30de0a5da7fc80f6bef0929cadb4fc88
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2020-12-26 21:25:33 +0300

    Каркас

--- RATORI.py
+++ RATORI.py
@@ -1,15 +1,16 @@
 #RATORI-game
-from modules import game
+from modules.Main import Main
 from pyautogui import *
 
-current_version = 2 #Запрос версии с интернета
+current_version = 1 #Запрос версии с интернета
 window_name = 'update'
 window_text = 'Вышла новая версия игры RaToRi'
+main = Main()
 
 if __name__ == '__main__':
-    local_version = game.local_version
+    local_version = main.local_version
     if local_version < current_version:
         print(window_text)
         alert(window_text, window_name, button='OK')
     else:
-        game.game_cycle()
+        main.game_cycle()
--- modules/Class1.py
+++ modules/Class1.py
@@ -1,9 +1,20 @@
 import pygame
 
 class class1(object):
+    pygame.init()
+    game_music = pygame.mixer.music.load('sounds\\audio.mp3')
+    sound = pygame.mixer.Sound('sounds\\sound.wav')
+    image = pygame.image.load('images\\BG.png')
+    font = pygame.font.SysFont('serif', 32)
+    msg = 'Hello World'
+    text = font.render(msg, True, 'grey')
 
     def draw(self, g):
         g.fill('brown')
+        pygame.mixer.Sound.play(self.sound)
+        g.blit(self.image, (125, 325))
+        pygame.mixer.music.play(-1)
+        g.blit(self.text, (300, 50))
         # pygame.draw.line(g, 'yellow', (50, 50), (500, 100), 1)
         # pygame.draw.aaline(g, 'yellow', (50, 150), (500, 200), 1)
         # pygame.draw.line(g, 'red', (250, 50), (300, 200), 1)
@@ -19,5 +30,5 @@ class class1(object):
         pygame.draw.rect(g, 'darkgreen', (350, 150, 100, 100))
         pygame.draw.rect(g, 'darkgreen', (375, 100, 50, 50))
 
-        # pygame.draw.lines(g, 'yellow', (100, 100), (200, 200), (100, 200), 5)
+        pygame.draw.lines(g, 'yellow', False, [(100, 100), (200, 200), (100, 200)], 5)
         pygame.draw.polygon(g, 'yellow', ((100, 200), (300, 10), (36, 80), (49, 341)), 5)
